8d088f30e90bd0902a6834d2226ed3e3f7dfe65a
Daniel <93489366+Altrocore@users.noreply.github.com>
2023-03-09 19:53:02 -0330

    Update ArrayList5.java

--- ArrayList5.java
+++ ArrayList5.java
@@ -6,18 +6,18 @@ import java.util.Collections;
 
 public class ArrayList5 {
     public static void main(String[] args) throws IOException {
-        ArrayList<Integer> list1 = new ArrayList<Integer>(); // Создание списка
-        Collections.addAll(list1, 1, 5, 6, 11, 3, 15, 7, 8); // Заполнение списка
+        ArrayList<Integer> list1 = new ArrayList<Integer>(); 
+        Collections.addAll(list1, 1, 5, 6, 11, 3, 15, 7, 8); 
 
         ArrayList<Integer> list2 = new ArrayList<Integer>();
         Collections.addAll(list2, 1, 8, 6, 21, 53, 5, 67, 18);
 
         ArrayList<Integer> result = new ArrayList<Integer>();
 
-        result.addAll(list1); // Добавление всех значений из одного списка в другой
+        result.addAll(list1);
         result.addAll(list2);
 
-        for (Integer x : result) // быстрый for по всем элементам, только для коллекций
+        for (Integer x : result) 
         {
             System.out.println(x);
         }
